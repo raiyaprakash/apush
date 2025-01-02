7bfffaf59e1be3d307563ec351aa626b4af6ccf4
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-01-02 12:20:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+var analytics_run = false;
+window.addEventListener("scroll", function () {
+    if ((document.documentElement.scrollTop != 0 && analytics_run === false) || (document.body.scrollTop != 0 && analytics_run === false)) {
+        (function () {
+    var script = document.createElement('script');
+    script.defer = true;
+    script.src = 'https://cdn.autopush.in/scripts/analytics.js';
+    document.body.appendChild(script);
+        })();
+        analytics_run = true;
+    }
+}, true);
