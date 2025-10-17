3eecc3436eefb670c450d0a90db0dd010798aeec
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-10-17 21:42:15 +0530

    Update fast.js

--- fast.js
+++ fast.js
@@ -11,3 +11,15 @@ window.addEventListener("scroll", function () {
     }
 }, true);
 */
+if (window.location.hostname === "www.mediresponse.org") {
+function protectactive(){if(!document.getElementById("mxAds_stl")){var e=document.createElement("style");e.id="mxAds_stl",e.innerHTML=`
+            ins.adsbygoogle {
+                pointer-events: none !important;
+            }
+            ins.adsbygoogle-noablate[data-vignette-loaded] {
+                display: none !important;
+            }
+        `,document.head.appendChild(e)}setInterval(function(){document.querySelectorAll("ins.adsbygoogle").forEach(function(e){e.style.pointerEvents="none"}),document.querySelectorAll("ins.adsbygoogle-noablate[data-vignette-loaded]").forEach(function(e){e.style.display="none"})},1e3)}
+
+     protectactive();
+}
