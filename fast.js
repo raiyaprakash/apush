7c3a37ebbf520ebf68a519734fa93bd49a765223
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-11-02 15:45:08 +0530

    Update fast.js

--- fast.js
+++ fast.js
@@ -1,28 +1,24 @@
-/* var analytics_run = false;
-window.addEventListener("scroll", function () {
-    if ((document.documentElement.scrollTop != 0 && analytics_run === false) || (document.body.scrollTop != 0 && analytics_run === false)) {
-        (function () {
-    var script = document.createElement('script');
-    script.defer = true;
-    script.src = 'https://cdn.autopush.in/scripts/analytics.js';
-    document.body.appendChild(script);
-        })();
-        analytics_run = true;
+if (window.location.hostname === "www.mediresponse.org" || window.location.hostname === "www.newsyhunt.com" || window.location.hostname === "www.indiakasamachar.com" || window.location.hostname === "teacherupdate.com" || window.location.hostname === "gyanbar.net" || window.location.hostname === "taazanaukari.com" || window.location.hostname === "www.updatemarts.in" || window.location.hostname === "www.gurujiportal.com") {
+    function protectactive() {
+        if (!document.getElementById("mxAds_stl")) {
+            var e = document.createElement("style");
+            e.id = "mxAds_stl", e.innerHTML = `
+ins.adsbygoogle2{pointer-events:none!important}body div.fc-message-root,ins.adsbygoogle-noablate,ins.adsbygoogle-noablate2[data-vignette-loaded],ins.adsbygoogle-noablate[data-anchor-shown=true]{display:none!important}
+        `, document.head.appendChild(e)
+        }
+        setInterval(function () {
+            document.body.style.overflow = 'auto';
+            document.querySelectorAll("ins.adsbygoogle2").forEach(function (e) {
+                e.style.pointerEvents = "none"
+            }), document.querySelectorAll("ins.adsbygoogle-noablate2[data-vignette-loaded]").forEach(function (e) {
+                e.style.display = "none"
+            }), document.querySelectorAll("ins.adsbygoogle-noablate2[data-vignette-loaded]").forEach(function (e) {
+                e.style.display = "none"
+            }), document.querySelectorAll("div.fc-message-root").forEach(function (e) {
+                e.style.display = "none";e.remove();
+            })
+        }, 1e3)
     }
-}, true);
-*/
-if (window.location.hostname === "www.mediresponse.org" || window.location.hostname === "www.newsyhunt.com") {
-function protectactive(){if(!document.getElementById("mxAds_stl")){var e=document.createElement("style");e.id="mxAds_stl",e.innerHTML=`
-            ins.adsbygoogle2 {
-                pointer-events: none !important;
-            }
-            ins.adsbygoogle-noablate{display: none !important;}
-            ins.adsbygoogle-noablate[data-anchor-shown="true"]{display: none !important;}
 
-            ins.adsbygoogle-noablate2[data-vignette-loaded] {
-                display: none !important;
-            }
-        `,document.head.appendChild(e)}setInterval(function(){document.querySelectorAll("ins.adsbygoogle2").forEach(function(e){e.style.pointerEvents="none"}),document.querySelectorAll("ins.adsbygoogle-noablate2[data-vignette-loaded]").forEach(function(e){e.style.display="none"})},1e3)}
-
-     protectactive();
+    protectactive();
 }
